9859ca53c16f9163fa42731968b15e758ab6e641
Abraham Bacor <abr.bacor@gmail.com>
2022-01-19 08:20:38 +0800

    Added: moment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("Hello world")
+//https://devhints.io/moment
+//https://momentjs.com/
+
+const moment = require('moment');
+let m = moment();
+//m.add(0, 'day')
+//m.subtract(0, 'days')
+console.log(
+  m.format('MMMM Do YYYY, h:mm:ss a')
+  //m.fromNow()
+);
